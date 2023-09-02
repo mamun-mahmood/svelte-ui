affe7df26e54dfdcf74f7f31b74b9e0ad1275d6c
mamun-mahmood <mamamun1999@gmail.com>
2023-09-02 13:29:03 +0600

    home page options for pages added

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,25 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="page-option">
+	<button>Layout</button>
+	<h1>/layout</h1>
+</div>
+<div class="page-option">
+	<button>Chart:</button>
+	<h1>/chart</h1>
+</div>
+<div class="page-option">
+	<button>Map:</button>
+	<h1>/map</h1>
+</div>
+
+<style>
+	.page-option {
+		display: flex;
+		align-items: center;
+		gap: 10px;
+	}
+	.page-option button {
+		width: 80px;
+		padding: 10px;
+		border-radius: 10px;
+	}
+</style>
