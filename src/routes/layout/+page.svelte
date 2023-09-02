04cdaa6b850225143d109a72add5f4bfbff1b082
mamun-mahmood <mamamun1999@gmail.com>
2023-09-02 18:30:29 +0600

    added layout page

--- src/routes/layout/+page.svelte
+++ src/routes/layout/+page.svelte
@@ -1,1 +1,13 @@
-<h1>layout</h1>
+<div class="grid grid-cols-2 h-screen">
+	<div class="grid grid-rows-6 grid-cols-6">
+		<div class="bg-blue-400 col-span-2 row-span-3" />
+		<div class="bg-rose-100 col-span-4 row-span-5" />
+		<div class="bg-teal-400 col-span-2 row-span-2" />
+		<div class="bg-fuchsia-400 row-span-1 col-span-6" />
+	</div>
+	<div class="grid grid-rows-6 grid-cols-6">
+		<div class="bg-blue-400 row-span-2 col-span-6" />
+		<div class="bg-slate-500 col-span-4 row-span-4" />
+		<div class="bg-rose-100 col-span-2 row-span-4" />
+	</div>
+</div>
